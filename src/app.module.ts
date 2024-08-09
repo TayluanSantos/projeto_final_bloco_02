ee9c0de63ef68448ed87f4abe6c8ce85008067b2
TayluanSantos <tayluanjesus@gmail.com>
2024-08-09 09:59:22 -0300

    feat: Implementando o recurso Categoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Categoria } from './categoria/entities/categoria.entity';
+import { CategoriaModule } from './categoria/categoria.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -11,11 +13,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'root',
       password: 'root',
       database: 'db_farmacia',
-      entities: [],
+      entities: [Categoria],
       synchronize: true, 
       logging: false,
       bigNumberStrings: false 
-  })],
+  }),
+  CategoriaModule],
   controllers: [],
   providers: [],
 })
